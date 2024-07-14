13099884ba04464bd18ec4867a517a4f57bfc6af
Purvik <purviksukhadiya99091@gmail.com>
2024-07-14 11:33:44 +0530

    home screen changed

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -16,44 +16,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
-    _fetchBooks();
     _searchController.addListener(_filterBooks);
-  }
-
-  void _fetchBooks() {
-    _booksRef.once().then((DatabaseEvent event) {
-      DataSnapshot snapshot = event.snapshot;
-      List<Map<String, dynamic>> fetchedBooks = [];
-      Map<dynamic, dynamic> values = snapshot.value as Map<dynamic, dynamic>;
-      values.forEach((key, value) {
-        fetchedBooks.add({
-          'id': key,
-          'isbn': value['isbn'] ?? '',
-          'title': value['title'] ?? '',
-          'authors': value['authors'] is List
-              ? (value['authors'] as List).join(', ')
-              : value['authors'] ?? '',
-          'publisher': value['publisher'] ?? '',
-          'publishedDate': value['publishedDate'] ?? '',
-          'description': value['description'] ?? '',
-          'pageCount': value['pageCount'] ?? 0,
-          'categories': value['categories'] is List
-              ? (value['categories'] as List).join(', ')
-              : value['categories'] ?? '',
-          'thumbnail': value['thumbnail'] ?? '',
-          'language': value['language'] ?? '',
-          'previewLink': value['previewLink'] ?? '',
-          'infoLink': value['infoLink'] ?? '',
-          'isIssued': value['isIssued'] ?? false,
-          'qty': value['qty'] ?? 0,
-          'totalIssuedQty': value['totalIssuedQty'] ?? 0,
-        });
-      });
-      setState(() {
-        _books = fetchedBooks;
-        _filteredBooks = fetchedBooks;
-      });
-    });
   }
 
   void _filterBooks() {
@@ -65,6 +28,36 @@ class _HomeScreenState extends State<HomeScreen> {
             book['categories'].toLowerCase().contains(query);
       }).toList();
     });
+  }
+
+  List<Map<String, dynamic>> _updateBooks(DataSnapshot snapshot) {
+    List<Map<String, dynamic>> fetchedBooks = [];
+    Map<dynamic, dynamic> values = snapshot.value as Map<dynamic, dynamic>;
+    values.forEach((key, value) {
+      fetchedBooks.add({
+        'id': key,
+        'isbn': value['isbn'] ?? '',
+        'title': value['title'] ?? '',
+        'authors': value['authors'] is List
+            ? (value['authors'] as List).join(', ')
+            : value['authors'] ?? '',
+        'publisher': value['publisher'] ?? '',
+        'publishedDate': value['publishedDate'] ?? '',
+        'description': value['description'] ?? '',
+        'pageCount': value['pageCount'] ?? 0,
+        'categories': value['categories'] is List
+            ? (value['categories'] as List).join(', ')
+            : value['categories'] ?? '',
+        'thumbnail': value['thumbnail'] ?? '',
+        'language': value['language'] ?? '',
+        'previewLink': value['previewLink'] ?? '',
+        'infoLink': value['infoLink'] ?? '',
+        'isIssued': value['isIssued'] ?? false,
+        'qty': value['qty'] ?? 0,
+        'totalIssuedQty': value['totalIssuedQty'] ?? 0,
+      });
+    });
+    return fetchedBooks;
   }
 
   @override
@@ -92,9 +85,25 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ),
           Expanded(
-            child: _filteredBooks.isEmpty
-                ? Center(child: CircularProgressIndicator())
-                : BookList(books: _filteredBooks),
+            child: StreamBuilder(
+              stream: _booksRef.onValue,
+              builder: (context, AsyncSnapshot<DatabaseEvent> snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(child: CircularProgressIndicator());
+                }
+
+                DataSnapshot dataSnapshot = snapshot.data!.snapshot;
+                _books = _updateBooks(dataSnapshot);
+                _filteredBooks = _books.where((book) {
+                  String query = _searchController.text.toLowerCase();
+                  return book['title'].toLowerCase().contains(query) ||
+                      book['authors'].toLowerCase().contains(query) ||
+                      book['categories'].toLowerCase().contains(query);
+                }).toList();
+
+                return BookList(books: _filteredBooks);
+              },
+            ),
           ),
         ],
       ),
@@ -125,50 +134,59 @@ class BookList extends StatelessWidget {
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(10),
             ),
-            child: ListTile(
-              leading: book['thumbnail'].isNotEmpty
-                  ? ClipRRect(
-                      borderRadius: BorderRadius.circular(8.0),
-                      child: Image.network(
-                        book['thumbnail'],
-                        width: 80,
-                        height: 120,
-                        fit: BoxFit.cover,
-                      ),
-                    )
-                  : Placeholder(fallbackWidth: 80, fallbackHeight: 120),
-              title: Text(
-                book['title'],
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text(book['authors']),
-              trailing: isAvailable
-                  ? ElevatedButton(
-                      onPressed: () {
-                        // Implement checkout functionality
-                      },
-                      child: Text('Checkout',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.green,
-                        shape: RoundedRectangleBorder(
+            child: Center(
+              child: Container(
+                height: 100, // Fixed height for the book card
+                child: ListTile(
+                  leading: book['thumbnail'].isNotEmpty
+                      ? ClipRRect(
                           borderRadius: BorderRadius.circular(8.0),
-                        ),
-                      ),
-                    )
-                  : ElevatedButton(
-                        onPressed: () {
-                          // Implement checkout functionality
-                        },
-                        child: Text('Not Available', style: TextStyle(color: Colors.white),),
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.red,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8.0),
+                          child: Image.network(
+                            book['thumbnail'],
+                            width: 80,
+                            height: 120,
+                            fit: BoxFit.cover,
+                          ),
+                        )
+                      : Placeholder(fallbackWidth: 80, fallbackHeight: 120),
+                  title: Text(
+                    book['title'],
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 3,
+                  ),
+                  subtitle: Text(
+                    book['authors'],
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1,
+                  ),
+                  trailing: isAvailable
+                      ? ElevatedButton(
+                          onPressed: () {
+                            // Implement checkout functionality
+                          },
+                          child: Text('Checkout',
+                              style: TextStyle(color: Colors.white)),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.green,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.0),
+                            ),
+                          ),
+                        )
+                      : ElevatedButton(
+                          onPressed: () {},
+                          child: Text('Not Available',
+                              style: TextStyle(color: Colors.white)),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.red,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.0),
+                            ),
                           ),
                         ),
-                      )
+                ),
+              ),
             ),
           ),
         );
