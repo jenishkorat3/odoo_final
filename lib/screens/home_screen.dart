b17fb7d251238606d38fed98dfab89c6ec930a1b
Purvik <purviksukhadiya99091@gmail.com>
2024-07-14 10:52:29 +0530

    book fetched with availability

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,5 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'package:odoo_hackathon/screens/auth/login_screen.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -14,11 +10,14 @@ class _HomeScreenState extends State<HomeScreen> {
   final DatabaseReference _booksRef =
       FirebaseDatabase.instance.ref().child('items');
   List<Map<String, dynamic>> _books = [];
+  List<Map<String, dynamic>> _filteredBooks = [];
+  TextEditingController _searchController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
     _fetchBooks();
+    _searchController.addListener(_filterBooks);
   }
 
   void _fetchBooks() {
@@ -46,39 +45,26 @@ class _HomeScreenState extends State<HomeScreen> {
           'previewLink': value['previewLink'] ?? '',
           'infoLink': value['infoLink'] ?? '',
           'isIssued': value['isIssued'] ?? false,
+          'qty': value['qty'] ?? 0,
+          'totalIssuedQty': value['totalIssuedQty'] ?? 0,
         });
       });
       setState(() {
         _books = fetchedBooks;
+        _filteredBooks = fetchedBooks;
       });
     });
   }
 
-  Future<Map<String, dynamic>> _fetchBookDetails(String isbn) async {
-    final response = await http.get(
-        Uri.parse('https://www.googleapis.com/books/v1/volumes?q=isbn:$isbn'));
-    if (response.statusCode == 200) {
-      final data = json.decode(response.body);
-      if (data['totalItems'] > 0) {
-        final book = data['items'][0]['volumeInfo'];
-        return {
-          'title': book['title'] ?? '',
-          'authors': book['authors'] != null ? book['authors'].join(', ') : '',
-          'publisher': book['publisher'] ?? '',
-          'publishedDate': book['publishedDate'] ?? '',
-          'description': book['description'] ?? '',
-          'pageCount': book['pageCount'] ?? 0,
-          'categories':
-              book['categories'] != null ? book['categories'].join(', ') : '',
-          'thumbnail':
-              book['imageLinks'] != null ? book['imageLinks']['thumbnail'] : '',
-          'language': book['language'] ?? '',
-          'previewLink': book['previewLink'] ?? '',
-          'infoLink': book['infoLink'] ?? '',
-        };
-      }
-    }
-    return {};
+  void _filterBooks() {
+    String query = _searchController.text.toLowerCase();
+    setState(() {
+      _filteredBooks = _books.where((book) {
+        return book['title'].toLowerCase().contains(query) ||
+            book['authors'].toLowerCase().contains(query) ||
+            book['categories'].toLowerCase().contains(query);
+      }).toList();
+    });
   }
 
   @override
@@ -86,18 +72,32 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Explore Your Library'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {
-              // Implement search functionality
-            },
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _searchController,
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search),
+                hintText: 'Search by title, author, or categories',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide.none,
+                ),
+                filled: true,
+                fillColor: Colors.grey[200],
+              ),
+            ),
+          ),
+          Expanded(
+            child: _filteredBooks.isEmpty
+                ? Center(child: CircularProgressIndicator())
+                : BookList(books: _filteredBooks),
           ),
         ],
       ),
-      body: _books.isEmpty
-          ? Center(child: CircularProgressIndicator())
-          : BookList(books: _books),
     );
   }
 }
@@ -112,31 +112,63 @@ class BookList extends StatelessWidget {
     return ListView.builder(
       itemCount: books.length,
       itemBuilder: (context, index) {
+        final book = books[index];
+        final isAvailable = book['qty'] - book['totalIssuedQty'] > 0;
+
         return InkWell(
           onTap: () {
             // Navigate to book details screen or expand details
           },
           child: Card(
             margin: EdgeInsets.all(8.0),
+            elevation: 5,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
             child: ListTile(
-              leading: books[index]['thumbnail'].isNotEmpty
-                  ? Image.network(
-                      books[index]['thumbnail'],
-                      width: 80,
-                      height: 120,
-                      fit: BoxFit.cover,
+              leading: book['thumbnail'].isNotEmpty
+                  ? ClipRRect(
+                      borderRadius: BorderRadius.circular(8.0),
+                      child: Image.network(
+                        book['thumbnail'],
+                        width: 80,
+                        height: 120,
+                        fit: BoxFit.cover,
+                      ),
                     )
                   : Placeholder(fallbackWidth: 80, fallbackHeight: 120),
-              title: Text(books[index]['title']),
-              subtitle: Text(books[index]['authors']),
-              trailing: books[index]['isIssued']
-                  ? Text('Unavailable')
-                  : ElevatedButton(
+              title: Text(
+                book['title'],
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(book['authors']),
+              trailing: isAvailable
+                  ? ElevatedButton(
                       onPressed: () {
                         // Implement checkout functionality
                       },
-                      child: Text('Checkout'),
-                    ),
+                      child: Text('Checkout',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                    )
+                  : ElevatedButton(
+                        onPressed: () {
+                          // Implement checkout functionality
+                        },
+                        child: Text('Not Available', style: TextStyle(color: Colors.white),),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.red,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                        ),
+                      )
             ),
           ),
         );
